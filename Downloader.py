3ee12ef4275c0588cb7721dedbca7ff4e9ec02b4
JuanEGamerYT <juanegameryt@gmail.com>
2024-02-07 13:10:39 -0300

    minor fixes

--- Downloader.py
+++ Downloader.py
@@ -1,7 +1,7 @@
 # -*- coding: cp1252 -*-
 import tkinter as tk
-from tkinter import filedialog
-from tkinter.ttk import Progressbar
+from tkinter import filedialog, messagebox
+from tkinter.ttk import Combobox
 from youtubesearchpython import Search
 import subprocess
 import webbrowser
@@ -13,7 +13,7 @@ def select_directory():
     directory_label.config(text=output_directory)
     update_download_button_state()
 
-def search_youtube():
+def search_youtube(*args):
     search_query = entry.get()
     allSearch = Search(search_query, limit=10)
     results = allSearch.result()['result']
@@ -23,14 +23,17 @@ def search_youtube():
         result_listbox.insert(tk.END, f"{i}. {result['title']}")
         video_ids.append(result['id'])
 
-def download_audio():
+def download_audio_or_video():
     selected_index = result_listbox.curselection()
     if selected_index:
         selected_id = video_ids[selected_index[0]]
         video_url = f"https://www.youtube.com/watch?v={selected_id}"
         ffmpeg_path = "FFmpeg/bin"  # Carpeta FFmpeg en el mismo directorio que el archivo
-        formatoelegido = format_combobox.get()
-        download_command = f"youtube-dl --ffmpeg-location {ffmpeg_path} -o {output_directory}%(title)s.%(ext)s --format {formatoelegido} {video_url}"
+        selected_format = format_combobox.get()
+        if selected_format == "Mejor Archivo de Video":
+            download_command = f"youtube-dl --ffmpeg-location {ffmpeg_path} -o {output_directory}%(title)s.%(ext)s -f bestvideo {video_url}"
+        else:
+            download_command = f"youtube-dl --ffmpeg-location {ffmpeg_path} -o {output_directory}/%(title)s.%(ext)s -x --audio-format {selected_format} {video_url}"
         subprocess.Popen(download_command, shell=True)
 
 def play_video():
@@ -43,29 +46,36 @@ def play_video():
 def update_download_button_state():
     if output_directory:
         download_button.config(state=tk.NORMAL)
-        directory_warning_label.config(text="")
     else:
         download_button.config(state=tk.DISABLED)
-        directory_warning_label.config(text="* Antes de descargar, selecciona un directorio", font=("Arial", 8), fg="gray")
 
-def update_format_button_state():
+def update_format_button_state(event=None):
     if format_combobox.get():
         download_button.config(state=tk.NORMAL)
-        format_warning_label.config(text="")
     else:
         download_button.config(state=tk.DISABLED)
-        format_warning_label.config(text="* Debes seleccionar en qué formato quieres el video", font=("Arial", 8), fg="gray")
+
+def on_enter_key_pressed(event):
+    search_youtube()
+
+def show_format_warning():
+    messagebox.showwarning("Advertencia", "Debes seleccionar un formato")
+
+def show_directory_warning():
+    messagebox.showwarning("Advertencia", "Antes de descargar, selecciona un directorio")
 
 window = tk.Tk()
-window.title("Descargar audio de YouTube")
+window.title("Descargar audio o video de YouTube")
 
 # Ajustar el tamaño de la ventana
-window_width = 600
+window_width = 500
 window_height = 300
 window.geometry(f"{window_width}x{window_height}")
+window.resizable(False, False)
 
 entry = tk.Entry(window, width=40)
 entry.pack(pady=10)
+entry.bind("<Return>", on_enter_key_pressed)
 
 search_button = tk.Button(window, text="Buscar en YouTube", command=search_youtube)
 search_button.pack()
@@ -77,30 +87,22 @@ button_frame = tk.Frame(window)
 button_frame.pack()
 
 directory_button = tk.Button(button_frame, text="Seleccionar directorio", command=select_directory)
-directory_button.pack(side=tk.LEFT)
+directory_button.grid(row=0, column=0)
 
-download_button = tk.Button(button_frame, text="Descargar", command=download_audio, state=tk.DISABLED)
-download_button.pack(side=tk.LEFT, padx=10)
+download_button = tk.Button(button_frame, text="Descargar", command=download_audio_or_video, state=tk.DISABLED)
+download_button.grid(row=0, column=1, padx=5)
+
+format_combobox = Combobox(button_frame, values=["Mejor Archivo de Video", "aac", "flac", "mp3", "m4a", "opus", "vorbis", "wav"], state="readonly")
+format_combobox.current(0)  # Establecer el valor predeterminado
+format_combobox.grid(row=0, column=2, padx=5)
+format_combobox.bind("<<ComboboxSelected>>", update_format_button_state)
 
 play_button = tk.Button(button_frame, text="Escuchar", command=play_video)
-play_button.pack(side=tk.LEFT)
+play_button.grid(row=0, column=3, padx=5)
 
 directory_label = tk.Label(window, text="Directorio de salida:")
 directory_label.pack()
 
-progress_bar = Progressbar(window, mode="determinate", maximum=100)
-progress_bar.pack(pady=10)
-
 video_ids = []
-
-format_combobox = tk.Combobox(window, values=["3gp", "aac", "flv", "m4a", "mp3", "mp4", "ogg", "wav", "webm"], state="readonly")
-format_combobox.current(0)  # Establecer el valor predeterminado
-format_combobox.pack()
-
-directory_warning_label = tk.Label(window, text="", font=("Arial", 8), fg="gray")
-directory_warning_label.pack()
-
-format_warning_label = tk.Label(window, text="", font=("Arial", 8), fg="gray")
-format_warning_label.pack()
 
 window.mainloop()
