41110a3a562d68486ac38a150ed17885a0a2d61f
JuanEGamerYT <juanegameryt@gmail.com>
2024-02-08 15:27:26 -0300

    adds coypright.

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,3 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Este script realiza la descarga de audio o video desde YouTube.
+
+Copyright (c) 2024 Juan Mellano (Fundador de Tormenta-Devs)
+
+Se concede permiso, de forma gratuita, a cualquier persona que obtenga una copia
+de este software y los archivos de documentación asociados (el "Software"), para
+tratar el Software sin restricciones, incluyendo, sin limitación, los derechos
+para usar, copiar, modificar, fusionar, publicar, distribuir, sublicenciar y/o vender
+copias del Software, y para permitir a las personas a las que se les proporcione el
+Software a hacerlo, con sujeción a las siguientes condiciones:
+
+El aviso de copyright anterior y este aviso de permiso se incluirán en todas las copias
+o porciones sustanciales del Software.
+
+EL SOFTWARE SE PROPORCIONA "TAL CUAL", SIN GARANTÍA DE NINGÚN TIPO, EXPRESA O IMPLÍCITA,
+INCLUYENDO PERO NO LIMITADO A LAS GARANTÍAS DE COMERCIABILIDAD, IDONEIDAD PARA UN PROPÓSITO
+PARTICULAR Y NO INFRACCIÓN. EN NINGÚN CASO LOS AUTORES O TITULARES DE LOS DERECHOS DE AUTOR
+SERÁN RESPONSABLES DE NINGÚN RECLAMO, DAÑO U OTRA RESPONSABILIDAD, YA SEA EN UNA ACCIÓN DE
+CONTRATO, AGRAVIO O DE OTRO MODO, DERIVADO DE, FUERA DE O EN CONEXIÓN CON EL SOFTWARE O EL USO
+U OTROS TRATOS EN EL SOFTWARE.
+
+Las copias ilegales de este software no están disponibles. Si desea realizar
+personalizaciones, por favor, realice un fork del repositorio en GitHub:
+https://github.com/Tormeta-Devs/youtube_downloader
+"""
 import tkinter as tk
 from tkinter import filedialog, messagebox
 from tkinter.ttk import Combobox
